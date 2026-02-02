77016c17d63ad503df4baaea0e6d90380d03dc95
carlosMartinx04 <carloscokchi04@gmail.com>
2026-02-02 20:22:22 +0100

    rwf

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,87 @@
 package org.example;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import java.lang.invoke.SwitchPoint;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+
+        //1. Abrir una sesión:
+        Session session = sessionFactory.openSession();
+        //2. Usar la sesión:
+        Transaction tx = session.beginTransaction();
+        // Operaciones...tx.commit();
+
+
+
+        //3. Cerrar la sesión:
+        session.close();
+
+    }
+
+    public static void mostrarMenu(){
+
+        System.out.println("Seleccione una opcion: ");
+        System.out.println("1. Insertar usuario." +
+                "2. Ver usuario." +
+                "3. Actualizar usuario." +
+                "4. Eliminar usuario" +
+                "5. Salir");
+
+        int opcion = sc.nextInt();
+
+        switch (opcion){
+            case 1:
+                insertarUsuarios();
+                mostrarMenu();
+                break;
+            case 2:
+                verUsuarios();
+                mostrarMenu();
+                break;
+            case 3:
+                actualizarUsuarios();
+                mostrarMenu();
+                break;
+            case 4:
+                eliminarUsuarios();
+                mostrarMenu();
+                break;
+            case 5:
+                break;
+            default:
+                mostrarMenu();
         }
+    }
+
+    public static void insertarUsuarios(){
+        String nombre, apellido;
+        int edad;
+
+        System.out.println("===========Insertar usuario===========");
+
+    }
+
+    public static void verUsuarios(){
+    }
+
+    public static void actualizarUsuarios(){
+
+    }
+
+    public static void eliminarUsuarios(){
+
     }
 }
---
+++ src/main/java/org/example/Usuario.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "nombre")
+    private String nombre;
+    @Column(name = "edad")
+    private int edad;
+    @Column(name = "email")
+    private String email;
+
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
